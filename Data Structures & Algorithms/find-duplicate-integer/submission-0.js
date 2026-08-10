d7e119068ae3a4a3b52baccf4c2192ff9ddcb66c
Chand ansari <chandansarithorium@gmail.com>
2026-08-10 16:58:29 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,9 @@
+class Solution {
+    findDuplicate(nums) {
+        let s = nums[0], f = nums[0];
+        do {s = nums[s]; f = nums[nums[f]];} while (s !== f);
+        s = nums[0];
+        while (s !== f) {s = nums[s]; f = nums[f];}
+        return s;
+    }
+}
