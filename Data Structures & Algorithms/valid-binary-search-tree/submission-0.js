3287f241f392e9e4b39120982ca6cf57b5f638c1
Chand ansari <chandansarithorium@gmail.com>
2026-08-14 10:21:52 +0530

    Add: valid-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-0.js
@@ -0,0 +1,10 @@
+class Solution {
+    isValidBST(root) {
+        const dfs = (node, min, max) => {
+            if(!node) return true;
+            if (node.val <= min || node.val >= max) return false;
+            return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
+        };
+        return dfs(root, -Infinity, Infinity);
+    }
+}
